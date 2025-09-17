1c394c472343ecc8bed0d6bc236ac779d18e88a9
Harpreet Kaur Binning <hkbinning@spartaglobal.com>
2025-09-17 19:06:46 +0100

    Set up working auth flow and account dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,31 @@
 import React from "react";
-import Nav from "./components/Nav";
-//import Homepage from "./Homepage";
-import "./App.css";
-import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/nav";
+import "./App.css";
+
+// Pages
 import Home from "./pages/Home";
 import Genre from "./pages/Genre";
 import SignInSignUp from "./pages/SignInSignUp";
 import Cart from "./pages/Cart";
 import Book from "./pages/Book";
 import MyAccount from "./pages/MyAccount";
+import { UserProvider } from "./context/UserProvider";
 
 export default function App() {
-  
-    const [user, setUser] = useState(() => {
-    const saved = localStorage.getItem("booksRUsUser");
-    return saved ? JSON.parse(saved) : null;
-  });
-  
-    useEffect(() => {
-    if (user) {
-      localStorage.setItem("booksRUsUser", JSON.stringify(user));
-    } else {
-      localStorage.removeItem("booksRUsUser");
-    }
-  }, [user]);
-
-    return (
-        <Router>
-            <Nav />
-            <Routes>
-                {/* Homepage */}
-                <Route path="/" element={<Home />} />
-
-                {/* Genre pages */}
-                <Route path="/genre/:genre" element={<Genre />} />
-                <Route path="/signin" element={<SignIn />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/book/:bookID" element={<Book />} />
-                          <Route
-          path="/signinsignup"
-          element={<SignInSignUp setUser={setUser} />}
-        />
-        <Route
-          path="/account"
-          element={<MyAccount user={user} setUser={setUser} />}
-        />
-            </Routes>
-        </Router>
-    );
+  return (
+    <UserProvider>
+      <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/genre/:genre" element={<Genre />} />
+        <Route path="/signinsignup" element={<SignInSignUp />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/book/:bookID" element={<Book />} />
+        <Route path="/account" element={<MyAccount />} />
+      </Routes>
+    </Router>
+    </UserProvider>
+  );
 }
