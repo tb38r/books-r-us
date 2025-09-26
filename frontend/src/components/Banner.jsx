c0e92fd6f1e8ccb2e7510308bbafa03916e70e4e
Harpreet Kaur Binning <hkbinning@spartaglobal.com>
2025-09-26 09:00:41 +0100

    banners

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import AuthorBanner from "./images/AuthorBanner.png"
+import "./Banner.css";
+import { Link } from "react-router-dom";
 
-
-
-export default function Banner({ image}) {
+export default function Banner({ image, alt, link }) {
   return (
-    <div className="banner">
-      <a >
-        <img src={AuthorBanner} alt="Banner" className="banner-img" />
-      </a>
+    <div className="banner-wrapper">
+      <Link to={link}>
+        <img src={image} alt={alt} className="banner-image" />
+      </Link>
     </div>
   );
 }
+
