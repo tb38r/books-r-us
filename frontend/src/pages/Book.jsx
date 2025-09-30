38c1658f8f49f3e3ef76f1a85af55cc206711417
ZaynahAC <zaynahac01@gmail.com>
2025-09-30 15:40:03 +0100

    Update Book.jsx from basket to cart line 339

--- frontend/src/pages/Book.jsx
+++ frontend/src/pages/Book.jsx
@@ -336,7 +336,7 @@ export default function Book() {
                                             },
                                         }}
                                     >
-                                        In Basket
+                                        In Cart
                                     </Button>
                                 ) : (
                                     <Button
